34b3f6123da2fe95efd96a6364240ad951aaabb5
jiaxiaogang <jxg2764894@live.com>
2025-09-14 18:20:25 +0800

    none

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/AIThinkingControl.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/AIThinkingControl.m
@@ -286,19 +286,15 @@ static AIThinkingControl *_instance;
     //[TIUtils recognitionGroupFeatureV4_Step2:jvBuModel];
     //NSLog(@"第3步、组特征竞争后条数:%ld",jvBuModel.gtModels.count);
     
-    
-    
-    
-    //TODOTOMORROW20250913: 一步步测特征，先测识别结果：
-    //此处有可能100条stModesls，来构建protoGT，这样肯定太长了。但如果只取前10条，会不会这10条都是0的右上角？得实测下。
-    
-    
     // 把局部识别结果打包成protoGT（参考35072-TODO2）。
     NSArray *orders = [SMGUtils convertArr:jvBuModel.stModels convertBlock:^id(AIFeatureJvBuModel *obj) {
         return [InputGroupFeatureModel new:obj.assT.p rect:obj.bestGVsAtProtoTRect];
     }];
     AIGroupFeatureNode *protoGT = [AIGeneralNodeCreater createGroupFeatureNode:orders conNodes:nil at:at ds:ds isOut:false isJiao:false];
     [protoGT updateLogDescItem:logDesc];
+    [SMGUtils runByMainQueue:^{
+        //[theApp.imgTrainerView setDataForFeature:protoGT lab:STRFORMAT(@"protoGT%ld",protoGT.pId) left:0 top:0];
+    }];
     NSLog(@"第3步、构建protoGT条数:%ld",protoGT.count);
     
     // 组特征识别：GT识别V5。
--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/In/TIUtils.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/In/TIUtils.m
@@ -227,7 +227,7 @@
         //52. debug (\t符合度:%.1f\t健全度:%.1f)
         NSLog(@"%ld. 单特征识别结果:T%ld%@\t 匹配条数:%ld/ass%ld %@",[decoratorJvBuModel.stModels indexOfObject:model],model.assT.pId,CLEANSTR([model.assT getLogDesc:true]),model.bestGVs.count,model.assT.count,model.getSTMatchDesc);
         [SMGUtils runByMainQueue:^{
-            //[theApp.imgTrainerView setDataForJvBuModelV2:model lab:STRFORMAT(@"%ld-识别单T%ld(%ld/%ld)",[decoratorJvBuModel.stModels indexOfObject:model]+1, model.assT.pId,model.bestGVs.count,model.assT.count) left:0 top:0];
+            [theApp.imgTrainerView setDataForJvBuModelV2:model lab:STRFORMAT(@"%ld-识别单T%ld(%ld/%ld)",[decoratorJvBuModel.stModels indexOfObject:model]+1, model.assT.pId,model.bestGVs.count,model.assT.count) left:0 top:0];
         }];
     }
     
@@ -583,7 +583,7 @@
         
         //45. 组特征识别结果可视化（参考34176）。
         [SMGUtils runByMainQueue:^{
-            [theApp.imgTrainerView setDataForFeature:assFeature lab:STRFORMAT(@"%ld-识别组T%ld",[resultModels indexOfObject:matchModel]+1,assFeature.pId) left:0 top:0];
+            //[theApp.imgTrainerView setDataForFeature:assFeature lab:STRFORMAT(@"%ld-识别组T%ld",[resultModels indexOfObject:matchModel]+1,assFeature.pId) left:0 top:0];
         }];
     }
     
