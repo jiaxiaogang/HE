1db5b832183d3b83a6a358cf51878e45458effa7
jiaxiaogang <jxg2764894@live.com>
2026-04-11 11:13:20 +0800

    写锚点计算算法（未完成）。

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/TCRecognition/ZiJvUtil.h
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/TCRecognition/ZiJvUtil.h
@@ -7,10 +7,45 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreGraphics/CoreGraphics.h>
+
+@class AIFeatureNode, AIFeatureJvBuItem;
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ZiJvUtil : NSObject
+
+/**
+ *  MARK:--------------------吸附公式--------------------
+ *  @param baseValue         本体值 (0~1)
+ *  @param envValue          环境值 (0~1)
+ *  @param envWeight         环境权重 (0~1)，权重越大，本体越趋向于环境值
+ *  @return                  吸附后的值 (0~1)
+ *  @note
+ *      公式: result = baseValue * (1 - envWeight) + envValue * envWeight
+ *      envWeight = 0: 本体值不变
+ *      envWeight = 1: 本体值完全变成环境值
+ */
++ (CGFloat) adsorbWithBaseValue_Single:(CGFloat)baseValue envValue:(CGFloat)envValue envWeight:(CGFloat)envWeight;
+
+/**
+ *  MARK:--------------------多环境吸附--------------------
+ *  @param baseValue         本体值 (0~1)
+ *  @param envs              环境数组 (NSArray<id>)
+ *  @param valueBlock        读取环境值的block
+ *  @param weightBlock       读取环境权重的block
+ *  @return                  各环境吸附结果的平均值
+ */
++ (CGFloat) adsorbWithBaseValue_Multi:(CGFloat)baseValue envs:(NSArray<id>*)envs valueBlock:(CGFloat(^)(id env))valueBlock weightBlock:(CGFloat(^)(id env))weightBlock;
+
+/**
+ *  MARK:--------------------计算吸附后的protoRect--------------------
+ *  @param bestGVs           本体bestGVs数组
+ *  @param baseT             本体AIFeatureNode
+ *  @param curIndex          本体下标
+ *  @return                  吸附后的protoRect
+ */
++ (CGRect) calcAdsorbProtoRect:(NSArray<AIFeatureJvBuItem*>*)bestGVs baseT:(AIFeatureNode*)baseT curIndex:(NSInteger)curIndex;
 
 @end
 
--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/TCRecognition/ZiJvUtil.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/TCRecognition/ZiJvUtil.m
@@ -7,7 +7,67 @@
 //
 
 #import "ZiJvUtil.h"
+#import "AIFeatureNode.h"
+#import "AIFeatureJvBuItem.h"
 
 @implementation ZiJvUtil
+
++ (CGFloat) adsorbWithBaseValue_Single:(CGFloat)baseValue envValue:(CGFloat)envValue envWeight:(CGFloat)envWeight {
+    // value和weight范围都是0~1
+    return baseValue * (1.0f - envWeight) + envValue * envWeight;
+}
+
++ (CGFloat) adsorbWithBaseValue_Multi:(CGFloat)baseValue envs:(NSArray<id>*)envs valueBlock:(CGFloat(^)(id env))valueBlock weightBlock:(CGFloat(^)(id env))weightBlock {
+    if (envs.count == 0) return baseValue;
+
+    CGFloat sum = 0;
+    for (id env in envs) {
+        CGFloat envValue = valueBlock(env);
+        CGFloat envWeight = weightBlock(env);
+        sum += [self adsorbWithBaseValue_Single:baseValue envValue:envValue envWeight:envWeight];
+    }
+    return sum / envs.count;
+}
+
++ (CGRect) calcAdsorbProtoRect:(NSDictionary*)bestGVs baseT:(AIFeatureNode*)baseT curIndex:(NSInteger)curIndex {
+    // 取整体宽高。
+    CGRect baseTRect = baseT.rect;
+    CGFloat width = baseTRect.size.width;
+    CGFloat height = baseTRect.size.height;
+    
+    // 取本体的四条边的归一化值。
+    CGRect curRect = [baseT rectByIndex:curIndex];
+    CGFloat curMinX = CGRectGetMinX(curRect) / width;
+    CGFloat curMaxX = CGRectGetMaxX(curRect) / width;
+    CGFloat curMinY = CGRectGetMinX(curRect) / height;
+    CGFloat curMaxY = CGRectGetMaxY(curRect) / height;
+    
+    // 取环境的四条边的归一化值，并计算吸附后的值。
+    CGFloat adsorCurMinX = [self adsorbWithBaseValue_Multi:curMinX envs:bestGVs.allKeys valueBlock:^CGFloat(NSNumber *key) {
+        CGRect bestRect = [baseT rectByIndex:key.integerValue];
+        return CGRectGetMinX(bestRect) / width;
+    } weightBlock:^CGFloat(NSNumber *key) {
+        CGRect bestRect = [baseT rectByIndex:key.integerValue];
+        CGFloat envValue = CGRectGetMinX(bestRect) / width;
+        AIFeatureJvBuItem *value = [bestGVs objectForKey:key];
+        CGFloat distanceWeight = 1.0 - fabs(envValue - curMinX);
+        return distanceWeight * value.matchValue;
+    }];
+    
+    CGFloat adsorCurMaxX = [self adsorbWithBaseValue_Multi:curMaxX envs:bestGVs.allKeys valueBlock:^CGFloat(NSNumber *key) {
+        CGRect bestRect = [baseT rectByIndex:key.integerValue];
+        return CGRectGetMaxX(bestRect) / width;
+    } weightBlock:^CGFloat(NSNumber *key) {
+        CGRect bestRect = [baseT rectByIndex:key.integerValue];
+        CGFloat envValue = CGRectGetMaxX(bestRect) / width;
+        AIFeatureJvBuItem *value = [bestGVs objectForKey:key];
+        CGFloat distanceWeight = 1.0 - fabs(envValue - curMaxX);
+        return distanceWeight * value.matchValue;
+    }];
+    
+    
+    // TODO: 实现吸附后的protoRect计算
+    return CGRectZero;
+}
 
 @end
