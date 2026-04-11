2505e21b231127adf2298a688e152d1db1597a4b
jiaxiaogang <jxg2764894@live.com>
2026-04-11 12:23:41 +0800

    写锚点计算算法（完成80%）。

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/TCRecognition/ZiJvUtil.h
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/TCRecognition/ZiJvUtil.h
@@ -11,52 +11,15 @@
 
 @class AIFeatureNode, AIFeatureJvBuItem;
 
-NS_ASSUME_NONNULL_BEGIN
-
 @interface ZiJvUtil : NSObject
 
 /**
- *  MARK:--------------------吸附公式--------------------
- *  @param baseValue         本体值 (0~1)
- *  @param envValue          环境值 (0~1)
- *  @param envWeight         环境权重 (0~1)，权重越大，本体越趋向于环境值
- *  @return                  吸附后的值 (0~1)
- *  @note
- *      公式: result = baseValue * (1 - envWeight) + envValue * envWeight
- *      envWeight = 0: 本体值不变
- *      envWeight = 1: 本体值完全变成环境值
- */
-+ (CGFloat) adsorbWithBaseValue_Single:(CGFloat)baseValue envValue:(CGFloat)envValue envWeight:(CGFloat)envWeight;
-
-/**
- *  MARK:--------------------多环境吸附--------------------
- *  @param baseValue         本体值 (0~1)
- *  @param envs              环境数组 (NSArray<id>)
- *  @param envBlock          返回环境值和权重的block @{@"value": @(值), @"weight": @(权重)}
- *  @return                  各环境吸附结果的平均值
- */
-+ (CGFloat) adsorbWithBaseValue_Multi:(CGFloat)baseValue envs:(NSArray<id>*)envs envBlock:(NSDictionary*(^)(id env))envBlock;
-
-/**
- *  MARK:--------------------计算单条边的吸附值--------------------
- *  @param baseT             本体AIFeatureNode
- *  @param curIndex          本体下标
- *  @param bestGVs           环境字典
- *  @param edgeFunc          边提取函数
- *  @param denominator       分母（宽或高）
- *  @return                  吸附后的边值
- */
-+ (CGFloat) calcAdsorbEdgeWithBaseT:(AIFeatureNode*)baseT curIndex:(NSInteger)curIndex bestGVs:(NSDictionary*)bestGVs edgeFunc:(CGFloat(^)(CGRect))edgeFunc denominator:(CGFloat)denominator;
-
-/**
- *  MARK:--------------------计算吸附后的protoRect--------------------
+ *  MARK:--------------------计算吸附后的assRect--------------------
  *  @param bestGVs           本体bestGVs字典
  *  @param baseT             本体AIFeatureNode
  *  @param curIndex          本体下标
  *  @return                  吸附后的protoRect
  */
-+ (CGRect) calcAdsorbProtoRect:(NSDictionary*)bestGVs baseT:(AIFeatureNode*)baseT curIndex:(NSInteger)curIndex;
++ (CGRect) calcAdsorbAssRect:(NSDictionary*)bestGVs baseT:(AIFeatureNode*)baseT curIndex:(NSInteger)curIndex;
 
 @end
-
-NS_ASSUME_NONNULL_END
--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/TCRecognition/ZiJvUtil.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/TCRecognition/ZiJvUtil.m
@@ -12,11 +12,29 @@
 
 @implementation ZiJvUtil
 
+/**
+ *  MARK:--------------------吸附公式--------------------
+ *  @param baseValue         本体值 (0~1)
+ *  @param envValue          环境值 (0~1)
+ *  @param envWeight         环境权重 (0~1)，权重越大，本体越趋向于环境值
+ *  @return                  吸附后的值 (0~1)
+ *  @note
+ *      公式: result = baseValue * (1 - envWeight) + envValue * envWeight
+ *      envWeight = 0: 本体值不变
+ *      envWeight = 1: 本体值完全变成环境值
+ */
 + (CGFloat) adsorbWithBaseValue_Single:(CGFloat)baseValue envValue:(CGFloat)envValue envWeight:(CGFloat)envWeight {
     // value和weight范围都是0~1
     return baseValue * (1.0f - envWeight) + envValue * envWeight;
 }
 
+/**
+ *  MARK:--------------------多环境吸附--------------------
+ *  @param baseValue         本体值 (0~1)
+ *  @param envs              环境数组 (NSArray<id>)
+ *  @param envBlock          返回环境值和权重的block @{@"value": @(值), @"weight": @(权重)}
+ *  @return                  各环境吸附结果的平均值
+ */
 + (CGFloat) adsorbWithBaseValue_Multi:(CGFloat)baseValue envs:(NSArray<id>*)envs envBlock:(NSDictionary *(^)(id))envBlock {
     if (envs.count == 0) return baseValue;
 
@@ -30,6 +48,15 @@
     return sum / envs.count;
 }
 
+/**
+ *  MARK:--------------------计算单条边的吸附值--------------------
+ *  @param baseT             本体AIFeatureNode
+ *  @param curIndex          本体下标
+ *  @param bestGVs           环境字典
+ *  @param edgeBlock         边提取函数
+ *  @param max               分母（宽或高）
+ *  @return                  吸附后的边值
+ */
 + (CGFloat) calcAdsorbEdgeWithBaseT:(AIFeatureNode*)baseT curIndex:(NSInteger)curIndex bestGVs:(NSDictionary*)bestGVs edgeBlock:(CGFloat(^)(CGRect))edgeBlock max:(CGFloat)max {
     CGFloat curValue = edgeBlock([baseT rectByIndex:curIndex]) / max;
     return [self adsorbWithBaseValue_Multi:curValue envs:bestGVs.allKeys envBlock:^NSDictionary *(NSNumber *key) {
@@ -41,21 +68,31 @@
     }];
 }
 
-+ (CGRect) calcAdsorbProtoRect:(NSDictionary*)bestGVs baseT:(AIFeatureNode*)baseT curIndex:(NSInteger)curIndex {
+/**
+ *  MARK:--------------------计算吸附后的protoRect--------------------
+ *  @param bestGVs           本体bestGVs字典
+ *  @param baseT             本体AIFeatureNode
+ *  @param curIndex          本体下标
+ *  @return                  吸附后的protoRect
+ */
++ (CGRect) calcAdsorbAssRect:(NSDictionary*)bestGVs baseT:(AIFeatureNode*)baseT curIndex:(NSInteger)curIndex {
     // 取整体宽高。
     CGRect baseTRect = baseT.rect;
     CGFloat width = baseTRect.size.width;
     CGFloat height = baseTRect.size.height;
-
+    
+    // 四条边分别计算吸附值，范围都是0~1。
     CGFloat adsorCurMinX = [self calcAdsorbEdgeWithBaseT:baseT curIndex:curIndex bestGVs:bestGVs edgeBlock:^CGFloat(CGRect rect) { return CGRectGetMinX(rect); } max:width];
     CGFloat adsorCurMaxX = [self calcAdsorbEdgeWithBaseT:baseT curIndex:curIndex bestGVs:bestGVs edgeBlock:^CGFloat(CGRect rect) { return CGRectGetMaxX(rect); } max:width];
     CGFloat adsorCurMinY = [self calcAdsorbEdgeWithBaseT:baseT curIndex:curIndex bestGVs:bestGVs edgeBlock:^CGFloat(CGRect rect) { return CGRectGetMinY(rect); } max:height];
     CGFloat adsorCurMaxY = [self calcAdsorbEdgeWithBaseT:baseT curIndex:curIndex bestGVs:bestGVs edgeBlock:^CGFloat(CGRect rect) { return CGRectGetMaxY(rect); } max:height];
     
     // 计算到吸附值后，根据四条边，再拼回rect返回。
-    
-    // TODO: 实现吸附后的protoRect计算
-    return CGRectZero;
+    CGFloat adsorX = adsorCurMinX * width;
+    CGFloat adsorY = adsorCurMinY * height;
+    CGFloat adsorW = (adsorCurMaxX - adsorCurMinX) * width;
+    CGFloat adsorH = (adsorCurMaxY - adsorCurMinY) * height;
+    return CGRectMake(adsorX, adsorY, adsorW, adsorH);
 }
 
 @end
